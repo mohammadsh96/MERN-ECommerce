b46f24bb1e3ec8fc2f0fe46f21614ac72f20904a
outsidett <outside@pathianlabs.com>
2022-12-14 21:01:56 +0000

    authentication token

--- index.js
+++ index.js
@@ -1,67 +1,26 @@
-const express =require('express')
-const app =express()
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const PORT =process.env.PORT || 3000
-const url = process.env.URL
-const users =require('./src/models/users')
-app.use(bodyParser.json())
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const url = process.env.URL;
+const { requireAuth, checkUser } = require('./src/middleware/middleware');
+
+const authRoutes = require("./src/routes/auth");
+
+app.use(express.json());
+
+
+// database connection
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(PORT ,()=>{
+    console.log(`Server is Running on Port :${PORT} and Database is Connected`);
+  }))
+  .catch((err) => console.log(err));
 
 
 
-const db = module.exports =()=>{
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-     
-}
-try {
-    mongoose.connect(url)
-    console.log("database Connected successfully");
-} catch (error) {
-    console.log(error);
-}
-}
-
-app.listen(PORT ,()=>{
-    console.log('server is Running on port : '+PORT);
-    db()
-})
-
-app.post('/users' , async (req,res)=>{
-    const user =new users()
-    const data =await users.findOne({username:req.body.username})
-    if(data){
-        res.send({message:'username is not avilable'})
-    }else{
-
-        user.username =req.body.username
-        user.password =req.body.password
-        user.role =req.body.role
-        console.log(req.body)
-    
-        user.save((err,data)=>{
-            if(err){
-                console.log(err)
-            }else{
-                res.status(200).send(data)
-            }
-        })
-    }
-})
-
-app.get('/users' , async (req,res)=>{
-    const user = await users.find()
-    res.status(200).send(user)
-    
-
- 
-})
-app.delete('/users' , async (req,res)=>{
-    const user = await users.deleteMany()
-    res.status(200).send(user)
-    
-
- 
-})
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.send('Welcome To Home Page ❤'));
+app.use(authRoutes);
--- src/models/users.js
+++ src/models/users.js
@@ -1,27 +1,49 @@
-const mongoose =require('mongoose')
-const UserModel =mongoose.Schema({
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
-    username: {
-        type: String,
-        dropDups:true,
-        required: true,
-        trim :true,
-        unique :true
-    },
-    password : {
-        type : String,
-        required: true,
-        trim :true
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  },
+  token:String
+});
 
-    },
-    role:{
-        type:String,
-        default :'user'
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+// fire a function before doc saved to db
+userSchema.post('save', async function(next) {
+  console.log('user has been created');
+  });
+
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      
+      return user;
     }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 
+const User = mongoose.model('user', userSchema);
 
-
-
-})
-const users= mongoose.model('users',UserModel)
-module.exports = users
+module.exports = User;
